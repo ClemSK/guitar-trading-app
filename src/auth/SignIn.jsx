190d744ddb3574802f41f1345ca5b23ffced5652
Dominic Jonas <ppkk.dominic@gmail.com>
2022-01-09 13:15:11 +0000

    added user collection, check for first time sign in and conditional new user doc creation

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -3,14 +3,24 @@ import { initializeApp } from 'firebase/app'
 import { firebaseConfig } from '../firebase'
 
 import {
+  getAdditionalUserInfo,
   getAuth,
   GoogleAuthProvider,
   signInWithPopup
 } from 'firebase/auth'
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp
+} from 'firebase/firestore'
 
 initializeApp(firebaseConfig)
 
+// refs
+const db = getFirestore()
 const auth = getAuth()
+const userColRef = collection(db, 'users')
 
 const SignIn = () => {
   const signInWithGoogle = () => {
@@ -20,8 +30,18 @@ const SignIn = () => {
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result)
         const token = credential.accessToken
-        // signed in user info
-        const user = result.user
+
+        // check if this is the users first sign in, if it is then create a new user doc to be referenced later, if it isnt, just sign and add no extra data
+        const details = getAdditionalUserInfo(result)
+        if (details.isNewUser) {
+          addDoc(userColRef, {
+            uid: auth.currentUser.uid,
+            displayName: auth.currentUser.displayName,
+            photoURL: auth.currentUser.photoURL,
+            email: auth.currentUser.email,
+            guitarsPosted: ['guitar 1', 'guitar 2']
+          })
+        }
       })
       .catch((err) => {
         console.log(err.message)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { firebaseConfig } from '../firebase'
+
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  orderBy
+} from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+initializeApp(firebaseConfig)
+
+// refs
+const db = getFirestore()
+const auth = getAuth()
+const userColRef = collection(db, 'users')
+
+const q = query(userColRef, orderBy('createdAt'))
 
 const Home = () => {
   return (
